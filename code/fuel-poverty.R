997fe0b05adac015a13a3f39893a86e3117e5cb3
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-10-16 12:51:13 +0100

    Fuel poverty

---
+++ code/fuel-poverty.R
@@ -0,0 +1,24 @@
+# Fuel poverty #
+
+# Source: Department for Business, Energy & Industrial Strategy
+# URL: https://www.gov.uk/government/statistics/sub-regional-fuel-poverty-data-2018
+# Licence: Open Government Licence 3.0
+
+library(tidyverse) ; library(httr) ; library(readxl)
+source("https://github.com/traffordDataLab/assets/raw/master/theme/ggplot2/theme_lab.R")
+
+tmp <- tempfile(fileext = ".xlsx")
+GET(url = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/731183/Fuel_poverty_sub-regional_tables_2018.xlsx",
+    write_disk(tmp))
+
+df <- read_xlsx(tmp, sheet = 6, range = "A3:H32847") %>% 
+  mutate(indicator = "Proportion of households in fuel poverty",
+         period = "2016",
+         measure = "Proportion",
+         unit = "Households") %>% 
+  select(lsoa11cd = `LSOA Code`, lsoa11nm = `LSOA Name`,
+         area_code = `LA Code`, area_name = `LA Name`,
+         indicator, period, measure, unit,
+         value = `Proportion of households fuel poor (%)`) 
+  
+write_csv(df, "../data/fuel_poverty.csv")
