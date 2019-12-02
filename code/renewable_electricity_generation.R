264a2b371bb85924860b9afdcf523f0f196ab0e4
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2019-12-02 15:23:47 +0000

    2018 data for generation from renewables

--- code/renewable_electricity_generation.R
+++ code/renewable_electricity_generation.R
@@ -9,17 +9,17 @@ library(tidyverse) ; library(httr) ; library(readxl)
 lookup <- read_csv("../data/geospatial/local_authority_codes.csv") %>% 
   pull(area_code)
 
-url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/743822/Renewable_electricity_by_Local_Authority__2014-2017.xlsx"
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/834142/Renewable_electricity_by_local_authority_2014_to_2018.xlsx"
 GET(url, write_disk(tmp <- tempfile(fileext = ".xlsx")))
 
-df <- read_excel(tmp, sheet = 10, skip = 2) %>% 
+df <- read_excel(tmp, sheet = 12, skip = 2) %>% 
   filter(`Local Authority Code4` %in% lookup) %>% 
   select(area_code = `Local Authority Code4`,
          area_name = `Local Authority Name`,
          6:17) %>% 
   gather(group, value, -area_code, -area_name) %>% 
   mutate(indicator = "Renewable electricity generation",
-         period = "2017-01-01",
+         period = "2018-01-01",
          measure = "Energy",
          unit = "MWh") %>% 
   select(area_code, area_name, indicator, period, measure, unit, value, group)
