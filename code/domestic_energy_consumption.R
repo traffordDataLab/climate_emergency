038e0914a68b44c8cb1a8626fff452630100dfa0
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2020-01-21 11:54:03 +0000

    Domestic energy consumption - 2017

--- code/domestic_energy_consumption.R
+++ code/domestic_energy_consumption.R
@@ -1,4 +1,4 @@
-# Domestic energy consumption (2016) #
+# Domestic energy consumption (2017) #
 
 # Source: BEIS
 # Publisher URL: https://www.gov.uk/government/statistical-data-sets/total-final-energy-consumption-at-regional-and-local-authority-level
@@ -8,21 +8,21 @@ library(tidyverse) ; library(httr) ; library(readxl)
 
 lookup <- read_csv("../data/geospatial/local_authority_codes.csv")
 
-url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/825965/Sub-national-total-final-energy-consumption-statistics-2005-2016-revised-19082019.xlsx"
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833987/Sub-national-total-final-energy-consumption-statistics_2005-2017.xlsx"
 GET(url, write_disk(tmp <- tempfile(fileext = ".xlsx")))
 
-df <- read_xlsx(tmp, sheet = 26, skip = 3) %>% 
-  filter(`LA Code (7)` %in% lookup$area_code) %>% 
-  select(area_code = `LA Code (7)`,
+df <- read_xlsx(tmp, sheet = 28, skip = 3) %>% 
+  filter(`LA Code` %in% lookup$area_code) %>% 
+  select(area_code = `LA Code`,
          area_name = `Government Office Regions and LAU1 Areas`,
          Coal = `Domestic...4`,
          `Manufactured fuels` = `Domestic...9`,
          `Petroleum products` = `Domestic...13`,
-         Gas = `Domestic...19`,
-         Electricity = `Domestic...23`) %>% 
+         Gas = `Domestic...21`,
+         Electricity = `Domestic...25`) %>% 
   gather(group, value, -area_code, -area_name) %>% 
   mutate(indicator = "Domestic energy consumption",
-         period = "2016-01-01",
+         period = "2017-01-01",
          measure = "Energy",
          unit = "Gigawatt hours (GWh") %>% 
   select(area_code, area_name, indicator, period, measure, unit, value, group)
