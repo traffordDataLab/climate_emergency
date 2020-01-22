08a9829295d936c788ce8cec695e6fd7e32850c9
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2020-01-22 12:40:55 +0000

    Corrected error in Domestic Renewable Heat Incentive dataset

--- code/domestic_rhi.R
+++ code/domestic_rhi.R
@@ -11,11 +11,18 @@ lookup <- read_csv("../data/geospatial/local_authority_codes.csv")
 url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/852760/RHI_monthly_official_stats_tables_Nov_19_final.xlsx"
 GET(url, write_disk(tmp <- tempfile(fileext = ".xlsx")))
 
-df <- read_xlsx(tmp, sheet = 20, skip = 4) %>% 
+# area_code column in worksheet 2.4 is incorrect so use area_code column from 1.4
+
+area_name_match <- read_xlsx(tmp, sheet = 7, skip = 4) %>% 
   filter(`Area Codes` %in% lookup$area_code) %>% 
   mutate(area_name = case_when(is.na(`...4`) ~ `...3`, TRUE ~ `...4`)) %>% 
-  select(area_code = `Area Codes`,
-         area_name,
+  select(area_code = `Area Codes`, area_name)
+
+df <- read_xlsx(tmp, sheet = 20, skip = 4) %>% 
+  mutate(area_name = case_when(is.na(`...4`) ~ `...3`, TRUE ~ `...4`)) %>% 
+  left_join(area_name_match, by = "area_name") %>% 
+  filter(`Area Codes` %in% lookup$area_code) %>% 
+  select(area_code, area_name,
          value = `Number of accredited installations`) %>% 
   mutate(indicator = "Domestic Renewable Heat Incentive scheme",
          period = "2014-04 to 2019-11",
@@ -23,6 +30,6 @@ df <- read_xlsx(tmp, sheet = 20, skip = 4) %>%
          unit = "Accredited installations",
          value = case_when(value %in% c("#", "^") ~ "NA", TRUE ~ value)) %>% 
   select(area_code, area_name, indicator, period, measure, unit, value) %>% 
-  filter(!is.na(area_name))
+  filter(!is.na(area_code))
 
 write_csv(df, "../data/domestic_rhi.csv")
