d1cee2e0ff01080c4e0fcfe3cbf2f852b1056655
James Austin <james.austin@trafford.gov.uk>
2020-09-08 12:35:01 +0100

    Accessibility improvements: document language, h1 heading, changing colours to new Lab style for improved contrast, adding alt text to council logo.

--- ui.R
+++ ui.R
@@ -1,23 +1,61 @@
-ui <- fluidPage(
+ui <- fluidPage(title = "Climate emergency slide pack",
+  # Set the language of the page - important for accessibility
+  tags$html(lang = "en-GB"),
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
-    tags$style(".shiny-notification {top: 50% !important; left: 50% !important; margin-top: -100px !important; color: #212121;")
+    tags$link(rel = "stylesheet", href = "styles.css"),
+    tags$style("
+                @import url('https://fonts.googleapis.com/css?family=Open+Sans%7CRoboto');
+               
+                .shiny-notification {
+                  top: 50% !important;
+                  left: 50% !important;
+                  margin-top: -100px !important;
+                  color: #212121;
+                }
+               
+                h1 {
+                  font-family: 'Roboto', sans-serif;
+                  color: #707070;
+                  display: inline-block;
+                }
+
+                a, a:hover, a:focus, a:visited {
+                  color: #046dc3;
+                }
+
+                a {
+                  text-decoration: none;
+                }
+
+                a:hover {
+                  text-decoration: underline;
+                }
+                
+                footer {
+                  position:fixed;
+                  text-align:center;
+                  left: 0; bottom:0;
+                  width:100%;
+                  z-index:1000;
+                  height:30px;
+                  padding: 5px 20px;
+                  background-color: #f5f9ff;
+                }
+               ")
   ),
-  titlePanel(
-    div(
-      class = "headerContainer",
-      a(
-        img(
-          src = "https://github.com/traffordDataLab/traffordDataLab.github.io/raw/master/images/trafford_council_logo_black_on_white_100px.png",
+  tags$header(
+    class = "headerContainer",
+    a(
+      img(
+        src = "https://www.trafforddatalab.io/images/trafford_council_logo_black_on_white_100px.png",
           style = "position: relative; top: -5px;",
-          height = 60
-        ),
-        href = "https://www.trafford.gov.uk",
-        target = "_blank"
+          height = 60,
+          alt = "Trafford Council"
       ),
-      "Climate emergency slide pack"
+      href = "https://www.trafford.gov.uk",
+      target = "_blank"
     ),
-    windowTitle = "Climate emergency slide pack"
+    h1("Climate emergency slide pack")
   ),
   fluidRow(
     div(
@@ -26,8 +64,14 @@ ui <- fluidPage(
         width = '100%',
         p("Last updated: 22 January 2020"),
         div(style="display:inline-block;",
-            a(img(src="_stripes_GLOBE---1850-2018-MO.png", style="width:100%;align:left;padding-top:10px;padding-bottom:10px"),
-            href = "https://showyourstripes.info", target = "_blank")),
+            a(
+              img(
+                src="_stripes_GLOBE---1850-2018-MO.png",
+                style="width:100%;align:left;padding-top:10px;padding-bottom:10px"
+                ),
+              href = "https://showyourstripes.info", 
+              target = "_blank")
+            ),
         em("Annual average global temperatures (1850-2018). Source: ", a(href = "https://showyourstripes.info", target = "_blank", "showyourstripes.info")),
         br(),br(),
         includeHTML("intro.html"),
@@ -41,7 +85,6 @@ ui <- fluidPage(
     ),
   br(),br(),br(),
   tags$footer(
-    fluidRow(
       "Developed in ",
       a(href = "https://cran.r-project.org/", target = "_blank", "R"),
       " by the ",
@@ -49,8 +92,6 @@ ui <- fluidPage(
       " under the ",
       a(href = "https://www.trafforddatalab.io/LICENSE.txt", target = "_blank", "MIT"),
       " licence"
-    ),
-    style = "position:fixed; text-align:center; left: 0; bottom:0; width:100%; z-index:1000; height:30px; color: #7C7C7C; padding: 5px 20px; background-color: #E7E7E7"
   )
 )
 
